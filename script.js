6cf71128d46d6dee0f8547bddae0f1cc2964c475
Zane Perry <zane@crosspeaksolutions.com>
2023-08-14 19:20:13 -0400

    Updates Made with ChatGPT 4

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-const container = document.getElementById('image-container');
-
-function generateRandomImage() {
-    const tileSize = 150;  // Set a fixed size for the images
-
-    const img = new Image();
-    // Add a cache buster to ensure unique image each time
-    img.src = `https://picsum.photos/${tileSize}?${Date.now() + Math.random()}`;
-    img.classList.add('img-tile');
-
-    return img;
-}
-
-function fillScreenWithImages() {
-    const tilesX = 20;  // Number of tiles per row
-    const tilesY = 20;  // Number of tiles per column
-
-    for (let i = 0; i < tilesX * tilesY; i++) {
-        container.appendChild(generateRandomImage());
+// Function to fetch a random image and append it to the container
+function fetchAndAppendImage(imageSize) {
+    console.log("Fetching a random image...");
+  
+    // Using Lorem Picsum as the source, since it doesn't require an API key
+    const imageUrl = `https://picsum.photos/${imageSize}`;
+  
+    fetch(imageUrl)
+      .then((response) => {
+        if (!response.ok) {
+          console.error("Error fetching the image:", response.status);
+          return;
+        }
+  
+        const img = new Image(imageSize, imageSize);
+        img.style.width = `${imageSize}px`;
+        img.style.height = `${imageSize}px`;
+        img.src = response.url;
+        document.getElementById('image-container').appendChild(img);
+  
+        console.log("Image fetched and appended:", img.src);
+      })
+      .catch((error) => console.error("Error fetching image:", error));
+  }
+  
+  // Function to calculate the number of images needed and fetch them
+  function populateImages() {
+    const containerWidth = window.innerWidth;
+    const containerHeight = window.innerHeight;
+  
+    // Calculate the exact size of the images to fit the screen perfectly
+    const columns = 10; // You can change this to set the number of columns
+    const imageSize = containerWidth / columns;
+    const rows = Math.ceil(containerHeight / imageSize);
+    const totalImages = rows * columns;
+  
+    console.log(`Total images required to fill the screen: ${totalImages}, Size of each image: ${imageSize}`);
+  
+    for (let i = 0; i < totalImages; i++) {
+      fetchAndAppendImage(imageSize);
     }
-}
-
-fillScreenWithImages();
-// Testing Push
+  }
+  
+  // Invoke the function on page load
+  populateImages();
+  
