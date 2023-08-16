7f51e841aed994803526aa74bedb7dc38fd21290
Zane Perry <zane@crosspeaksolutions.com>
2023-08-16 13:21:54 -0400

    Fixed Tiles.  All images perfectly fit now.

--- script.js
+++ script.js
@@ -4,37 +4,41 @@ function populateImages() {
   const containerWidth = window.innerWidth;
   const containerHeight = window.innerHeight;
 
-  // Number of rows and columns to divide the screen into
-  const rows = 5;
-  const cols = 5;
+  console.log(`Container width: ${containerWidth}, Container height: ${containerHeight}`);
 
-  // Calculating the exact width and height of each image tile
-  const imgWidth = Math.floor(containerWidth / cols);
-  const imgHeight = Math.floor(containerHeight / rows);
+  // Determine the size of each square tile to fit the width perfectly
+  const cols = Math.floor(containerWidth / 100); // Assuming each tile is 100 pixels
+  const imgSize = containerWidth / cols;
+
+  console.log(`Number of columns: ${cols}, Size of each tile: ${imgSize}`);
+
+  // Number of rows to divide the screen into
+  const rows = Math.floor(containerHeight / imgSize);
+
+  console.log(`Number of rows: ${rows}`);
 
   // Total images required to fill the screen
   const totalImages = rows * cols;
 
-  console.log(`Image size: ${imgWidth}x${imgHeight}, Total images: ${totalImages}`);
+  console.log(`Total images: ${totalImages}`);
 
   for (let i = 0; i < totalImages; i++) {
     // Creating an image element
     const img = document.createElement('img');
-    img.width = imgWidth;
-    img.height = imgHeight;
+    img.width = imgSize;
+    img.height = imgSize;
 
     // Setting the source URL with the rounded width and height
     // Adding a random parameter to ensure a new random image is fetched
     const randomParam = Math.random();
-    img.src = `https://picsum.photos/${Math.round(imgWidth)}/${Math.round(imgHeight)}?random=${randomParam}`;
+    img.src = `https://picsum.photos/${Math.round(imgSize)}/${Math.round(imgSize)}?random=${randomParam}`;
 
     // Appending the image to the container
     container.appendChild(img);
 
-    console.log(`Image ${i + 1} appended with size ${imgWidth}x${imgHeight}`);
+    console.log(`Image ${i + 1} appended with size ${imgSize}x${imgSize}`);
   }
 }
 
 // Invoke the function on page load
 populateImages();
-
